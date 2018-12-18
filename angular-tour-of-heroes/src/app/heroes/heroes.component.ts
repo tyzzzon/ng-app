f7ea28b0a1bf2df162f21a8cc9a6c30c502344e0
tyzzzon <tyzzon@yandex.ru>
2018-12-18 18:52:12 +0700

    add new component, add class, event and property binding, use @Input decorator

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,10 +9,13 @@ import { HEROES } from "../mock-heroes";
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
   heroes = HEROES;
 
   constructor() { }
