528d575746fa596b016aa55612e1741f563412b5
tyzzzon <tyzzon@yandex.ru>
2018-12-27 15:37:25 +0700

    add in memory data service, import httpclient. TODO: get heroes with HttpClient

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,11 +3,19 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
+
+  private heroesUrl = 'api/heroes'; // URL to web api
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
@@ -21,5 +29,8 @@ export class HeroService {
     return of(HEROES.find(hero => hero.id === id));
   }
 
-  constructor(private messageService: MessageService) { }
+  /** Log a HeroService message with the MessageService*/
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 }
